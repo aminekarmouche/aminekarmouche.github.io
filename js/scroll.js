0145de0b5c229b85c5d9c8d2d1688232a9a8bf97
Amine Karmouche <amine.karmouche@gmail.com>
2015-07-15 00:27:13 +0000

    animate title and scoll

--- js/scroll.js
+++ js/scroll.js
@@ -3,3 +3,9 @@ $("#resume").click(function() {
         scrollTop: $("#tab-1").offset().top
     }, 1000);
 });
+
+$("i").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#tab-1").offset().top
+    }, 1000);
+});
